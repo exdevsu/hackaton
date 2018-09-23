12042bf6188d77b508635fdf4b92e4f6166d6348
/usr/bin/vim <support@beget.ru>
2018-09-23 12:58:34 +0300

    Install Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+@section('content')
+    <header>
+        <!--Navbar-->
+        <nav class="navbar no-shadow scrolling-navbar navbar-expand-lg my-5 fixed-top navbar-dark">
+            <div class="container">
+                <!-- Navbar brand -->
+                <a class="navbar-brand h1-responsive animated fadeInDownBig" href="{{ route('home') }}">NaviMerch</a>
+
+                <!-- Collapse button -->
+                <button class="navbar-toggler animated fadeInDownBig" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Collapsible content -->
+                <div class="collapse navbar-collapse" id="basicExampleNav">
+
+                    <!-- Links left -->
+                    <ul class="navbar-nav mr-auto"></ul>
+                    <!-- Links left -->
+
+                    <form class="form-inline">
+                        <div class="md-form my-0">
+                            <ul class="navbar-nav mr-auto h5-responsive animated fadeInDownBig">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('home') }}">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('home') }}">FAQ</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </form>
+                </div>
+                <!-- Collapsible content -->
+            </div>
+        </nav>
+        <!--/.Navbar-->
+        <!-- Full Page Intro -->
+        <div class="view intro">
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-gradient d-flex justify-content-center align-items-center">
+                <!-- Content -->
+                <div class="container">
+                    <!--Grid row-->
+                    <div class="row mt-5">
+                        <!--Grid column-->
+                        <div class="col-md-10 my-5 mb-5 mt-md-0 mt-5 white-text text-center text-md-left center animated fadeInUp">
+                            <div class="text-center">
+                                <h1 class="h1-responsive">Welcome new merch with using <a href="https://naviaddress.com/" class="blue-text" target="_blank">NaviAddress</a>!</h1>
+                                <hr class="hr-light">
+                                <h6 class="mb-3">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga
+                                    nesciunt dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea
+                                    dolor molestiae, quisquam iste, maiores. Nulla.
+                                </h6>
+                            </div>
+                        </div>
+                        <!--Grid column-->
+                        <div class="col-md-12 col-xl-5 mb-4 center">
+                            <!--Form with header-->
+                            <div class="card animated fadeInUp">
+                                <div class="card-body">
+                                    <!--Header-->
+                                    <div class="form-header blue-gradient">
+                                        <h3><i class="fa fa-user mt-2 mb-2"></i> Log in</h3>
+                                    </div>
+                                    <!--Body-->
+                                    <div class="md-form">
+                                        <i class="fa fa-envelope prefix white-text"></i>
+                                        <input type="text" id="loginForm-Email" class="form-control grey-text">
+                                        <label for="loginForm-Email" >Your email</label>
+                                    </div>
+                                    <div class="md-form">
+                                        <i class="fa fa-lock prefix white-text"></i>
+                                        <input type="password" id="loginForm-pass" class="form-control grey-text">
+                                        <label for="loginForm-pass">Your password</label>
+                                    </div>
+                                    <div class="text-center">
+                                        <button class="btn blue-gradient btn-lg">Sign in</button>
+                                        <hr>
+                                        <div class="inline-ul text-center d-flex justify-content-center">
+                                            <a class="p-2 m-2 fa-lg tw-ic"><i class="fa fa-twitter white-text"></i></a>
+                                            <a class="p-2 m-2 fa-lg li-ic"><i class="fa fa-linkedin white-text"> </i></a>
+                                            <a class="p-2 m-2 fa-lg ins-ic"><i class="fa fa-instagram white-text"> </i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--/Form with header-->
+                        </div>
+                        <!--Grid column-->
+                    </div>
+                    <!--Grid row-->
+                </div>
+                <!-- Content -->
+            </div>
+            <!-- Mask & flexbox options-->
+        </div>
+        <!-- Full Page Intro -->
+    </header>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>NaviMerch - @yield('title')</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    {{ HTML::style('css/bootstrap.min.css') }}
+    {{ HTML::style('css/mdb.min.css') }}
+    {{ HTML::style('css/style.css') }}
+</head>
+<body>
+    
+    @yield('content')
+
+    <footer>
+        {{ HTML::script('js/jquery-3.3.1.min.js') }}
+        {{ HTML::script('js/bootstrap.min.js') }}
+        {{ HTML::script('js/mdb.min.js') }}
+        {{ HTML::script('js/popper.min.js') }}
+        {{ HTML::script('js/modules/wow.js') }}
+        {{ HTML::script('js/app.js') }}
+    </footer>
+</body>
+</html>
