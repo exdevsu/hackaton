3c93fe80e812d1ce722b76518a96ab4ce4e5dff6
pmswga <pmswga@gmail.com>
2018-09-24 20:02:15 +0300

    Работа над моделями
    - Создание модели для работы с зонами
    - Настройка модели User для аутентификации

--- app/User.php
+++ app/User.php
@@ -10,20 +10,13 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table      = 'Accounts';
+    protected $primaryKey = 'id';
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'second_name', 'first_name', 'patronymic', 'email', 'password'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
